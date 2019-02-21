6c1f40cd228f964e9d651f46f3dc60198a0e5429
alextimm007 <alextimm007@gmail.com>
2019-02-21 09:02:56 -0600

    database connection

---
+++ database_connection.php
@@ -0,0 +1,8 @@
+<?php 
+// file to connect to database
+$db_host = "localhost"; //can be "localhost" for local development
+$db_username = "root";
+$db_password = "";
+$db_name = "ics499";
+$conn = new mysqli($db_host,$db_username,$db_password,$db_name) or die(mysqli_error());
+?>
